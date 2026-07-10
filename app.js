626e763aafb95184737f02dc04d3f20c4e5e4d5f
Pantgom01 <pantgom01@educarex.es>
2026-07-10 11:29:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
 
+    console.log("DOM cargado");
+
     const input = document.getElementById("archivo");
     const visor = document.getElementById("visor");
+
+    console.log("input:", input);
+    console.log("visor:", visor);
 
     let urlActual = null;
 
     input.addEventListener("change", function () {
+
+        console.log("Cambio de archivo");
 
         const archivo = this.files[0];
 
@@ -17,10 +24,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
         urlActual = URL.createObjectURL(archivo);
 
+        console.log("URL:", urlActual);
+
+        console.log("Antes de asignar modelo");
+
         visor.setAttribute("gltf-model", urlActual);
+
         visor.setAttribute("visible", true);
 
-        console.log(urlActual);
+        console.log("Modelo asignado");
 
     });
 
